7e0490701771ef1220dcad7d6f2f287cc6ceb298
oredola <oredolaolamide@gmail.com>
2019-12-20 11:48:08 +0100

    Users can edit colors

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -21,6 +21,10 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axios()
+      .put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
   };
 
   const deleteColor = color => {
