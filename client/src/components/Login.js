ada6dc49c0fc0c7b1da21a685b38c9919135eb5b
oredola <oredolaolamide@gmail.com>
2019-12-20 10:26:24 +0100

    Added login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+const [formValues, setFormValues] = useState({
+  username: "Lambda School",
+  password: "i<3Lambd4"
+});
+const [error, setError] = useState(null);
+
+const handleChange = event => {
+  setFormValues({
+    ...formValues,
+    [event.target.name]: event.target.value
+  });
+};
+
+const handleSubmit = evt => {
+  evt.preventDefault();
+  axios
+    .post("http://localhost:5000/api/login", formValues)
+    .then(res => {
+      localStorage.setItem("token", res.data.payload);
+      // props.history.push("/friends");
+    })
+    .catch(error => {
+      console.log(error);
+      setError(error);
+    });
+};
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
       <p>Build a login page here</p>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          Username
+          <input
+            onChange={handleChange}
+            type="text"
+            name="username"
+            value={formValues.username}
+          />
+        </div>
+        <div>
+          Password
+          <input
+            onChange={handleChange}
+            type="password"
+            name="password"
+            value={formValues.password}
+          />
+        </div>
+        {error ? <p>Invalid Password. Retry!</p> : null}
+        <button type="submit">Login</button>
+      </form>
     </>
   );
 };
